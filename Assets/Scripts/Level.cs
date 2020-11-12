cadc525c01e2c4aa3edccd3a1b1725691407de93
Jeff Machusko <jeff.machusko@gmail.com>
2020-11-12 16:24:47 -0700

    Pipe Inversion

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,17 +9,31 @@ public class Level : MonoBehaviour
     private const float PIPE_HEAD_OFFSET = 1.8f;
     private void Start()
     {
-
+        CreatePipe(40f, 20, true);
+        CreatePipe(40f, 20, false);
     }
-    private void CreatePipe(float height, float xPosition)
+    private void CreatePipe(float height, float xPosition, bool createBottom)
     {
         // Create Head
         Transform pipeHead = Instantiate(GameAssets.GetInstance().PFPipeHead);
-        pipeHead.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_OFFSET);
+        float yHeadPosition = (createBottom) ? -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_OFFSET : CAMERA_ORTHO_SIZE - height + PIPE_HEAD_OFFSET;
+        pipeHead.position = new Vector3(xPosition, yHeadPosition);
 
         // Create Body
         Transform pipeBody = Instantiate(GameAssets.GetInstance().PFPipeBody);
-        pipeBody.position = new Vector3(xPosition, -CAMERA_ORTHO_SIZE);
+
+        float yBodyPosition;
+        if(createBottom)
+        {
+            yBodyPosition = -CAMERA_ORTHO_SIZE;
+        }
+        else
+        {
+            yBodyPosition = CAMERA_ORTHO_SIZE;
+            pipeBody.localScale = new Vector3(1, -1, 1);
+        }
+
+        pipeBody.position = new Vector3(xPosition, yBodyPosition);
         
         var bodyRenderer = pipeBody.GetComponent<SpriteRenderer>();
         bodyRenderer.size = new Vector2(PIPE_WIDTH, height);
